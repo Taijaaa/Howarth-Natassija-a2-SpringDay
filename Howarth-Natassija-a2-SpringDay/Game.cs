ac3d473ac368d8539ebf840c67bd80cd232c8718
Taijaaa <000903701@mohawkcollege.ca>
2025-02-17 12:27:25 -0500

    added colours

--- Howarth-Natassija-a2-SpringDay/Game.cs
+++ Howarth-Natassija-a2-SpringDay/Game.cs
@@ -12,6 +12,17 @@ namespace MohawkGame2D
     {
         // Place your variables here:
 
+        Color GreenGrass = new Color("ace1af");
+        Color BunnyBody = new Color("d8bab4");
+        Color PinkBody = new Color("d98584");
+        Color SunYellow = new Color("eeb422");
+        Color Rainblue = new Color("839db3");
+        Color GreyCloud = new Color("e7e7e7");
+        Color SkyBlue = new Color("cfe2f3");
+        Color NoseBrown = new Color("574332");
+        Color Black = new Color("000000");
+
+
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
